ddf3e1e474a114afe65f9804f468d955de205cc6
Ranjitha1304 <ranjitha13cs@gmail.com>
2025-10-04 03:17:23 +0530

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3fu3^&bldo4zpgotg-s)5@%9sm#ulw7-7_=+-^fjvqwhgrk@&r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
